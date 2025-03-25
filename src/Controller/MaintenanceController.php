e3cca87d480a976619b1fbdb9a330d6ea6aac048
cbk-nico <n.meyer@codeblick.de>
2025-03-25 16:27:58 +0100

    chore: update controller routing

--- src/Controller/MaintenanceController.php
+++ src/Controller/MaintenanceController.php
@@ -8,15 +8,13 @@ use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Shopware\Core\Framework\Adapter\Cache\CacheClearer;
 use Shopware\Storefront\Controller\StorefrontController;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
-/**
- * @Route(defaults={"_routeScope"={"storefront"}})
- */
+#[Route(defaults: ['_routeScope' => ['storefront']])]
 class MaintenanceController extends StorefrontController
 {
     private Connection $connection;
@@ -28,9 +26,6 @@ class MaintenanceController extends StorefrontController
      */
     private $salesChannelRepository;
 
-    /**
-     * @param Connection $connection
-     */
     public function __construct(
         Connection $connection,
         CacheClearer $cacheClearer,
@@ -42,13 +37,7 @@ class MaintenanceController extends StorefrontController
         $this->salesChannelRepository = $salesChannelRepository;
     }
 
-    /**
-     * @Route("/maintenance/login", name="frontend.maintenance.login", methods={"POST"}, defaults={"XmlHttpRequest"=true, "csrf_protected"=false})
-     *
-     * @param Request $request
-     * @param SalesChannelContext $context
-     * @return Response
-     */
+    #[Route(path: '/maintenance/login', name: 'frontend.maintenance.login', methods: ['POST'], defaults: ['XmlHttpRequest' => true, 'csrf_protected' => false])]
     public function maintenanceLogin(Request $request, SalesChannelContext $context): Response
     {
         if (!$this->verifyUser($request)) {
@@ -64,16 +53,11 @@ class MaintenanceController extends StorefrontController
         return new JsonResponse(['success' => $success], 200);
     }
 
-    /**
-     * @param Request $request
-     * @return boolean
-     */
     private function verifyUser(Request $request): bool
     {
         if (is_null($request->get('username')) || is_null($request->get('password'))) {
             return false;
         }
-
 
         $password = $this->connection->fetchOne(
             'SELECT `password` FROM user WHERE username = :username',
@@ -82,7 +66,6 @@ class MaintenanceController extends StorefrontController
             ]
         );
 
-
         if ($password === false || !password_verify($request->get('password'), $password)) {
             return false;
         }
@@ -90,11 +73,6 @@ class MaintenanceController extends StorefrontController
         return true;
     }
 
-    /**
-     * @param Request $request
-     * @param SalesChannelContext $context
-     * @return bool
-     */
     private function addIpToAllowedList(Request $request, SalesChannelContext $context): bool
     {
         $ipWhitelist = $context->getSalesChannel()->getMaintenanceIpWhitelist();
